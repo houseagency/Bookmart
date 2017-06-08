720250e6ccc916cb043218edc5ba8a8bb3e7385b
Mart van de Moosdijk <martvandemoosdijk@gmail.com>
2017-06-08 13:22:45 +0200

    Added some comments

--- src/server.js
+++ src/server.js
@@ -1,3 +1,7 @@
+/**
+ * Imports
+ */
+
 const server = require('express')();
 const bodyParser = require('body-parser');
 const {promisify} = require('util');
@@ -6,10 +10,19 @@ const redis = require('redis').createClient({
     host: process.env.REDIS_HOST
 });
 
+/**
+ * Constants
+ */
+
 const REDIS_KEY = 'bookmarks';
+
 const hset = promisify(redis.hset.bind(redis));
 const hgetall = promisify(redis.hgetall.bind(redis));
 const hdel = promisify(redis.hdel.bind(redis));
+
+/**
+ * Resolvers
+ */
 
 server.use(bodyParser.text());
 
@@ -44,6 +57,10 @@ server.delete('/bookmarks/:name', (req, res, next) => {
             res.sendStatus(500);
         })
 });
+
+/**
+ * Start server
+ */
 
 server.listen(process.env.PORT, () => {
     console.log('Server running on port', process.env.PORT);
